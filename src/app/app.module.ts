a8561e4321dc3cd173e56a1ead2b2714d6e386fe
Mabu4 <max.budziat@gmx.de>
2022-05-10 12:58:53 +0200

    tested databinding and ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
+
+@NgModule({ //ngModule ist nur dafür da, um Angular im Hintergrund mitzuteilen, welche generellen Bestandteile genutzt werden
+  declarations: [ //welche Bestandteile hat unsere App eigentlich
+    AppComponent, DatabindingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //von all diesen Bestandteilen, welche ich eigentlich die root Component (index.html)
 })
 export class AppModule { }
